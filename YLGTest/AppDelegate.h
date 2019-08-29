d1c682246c101b301e13f848f74660321892800a
yanglingui <840355064@qq.com>
2019-08-29 15:57:49 +0800

    Initial Commit

---
+++ YLGTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YLGTest
+//
+//  Created by yanglingui on 2019/8/29.
+//  Copyright © 2019 GDT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YLGTest/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  YLGTest
+//
+//  Created by yanglingui on 2019/8/29.
+//  Copyright © 2019 GDT. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
